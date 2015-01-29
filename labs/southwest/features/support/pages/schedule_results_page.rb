5ea30857da5b1148285dbcabe55e60ac91722222
c:\atdd_training <dtan2@netjets.com>
2015-01-29 17:01:21 -0500

    gather data

--- labs/southwest/features/support/pages/schedule_results_page.rb
+++ labs/southwest/features/support/pages/schedule_results_page.rb
@@ -3,18 +3,26 @@ class ScheduleResultsPage
   table(:schedule_table,:class=>'swa_feature_flightStatus_results_table swa_tables_grayTable2')
 
   def gather_schedule
-     data=[]
-    schedule_table_element.tbody.rows.each do |rows|
 
-      data << rows.text
+    schedule_table_element.tbody.rows.to_a.map{|rows|rows.text}
+
     end
      puts data
   end
 
-  def collect_header
-    schedule_table_element[1].to_a.each do |header|
-      header.text
-    end
+  def collect_rows row
+    schedule_table_element[row].to_a.map{|header| header.text}
+
   end
+
+
+
+
+  def create_hash
+
+
+
+  end
+
 
 end
