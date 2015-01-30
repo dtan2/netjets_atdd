62d5e340d593f93271d5bc56d92ca2781aec1146
c:\atdd_training <dtan2@netjets.com>
2015-01-30 15:10:07 -0500

    env

--- labs/southwest/features/support/hook.rb
+++ labs/southwest/features/support/hook.rb
@@ -3,7 +3,7 @@ require 'page-object'
 include PageObject::PageFactory
 
 Before do |scenario|
-@browser = Watir::Browser.new :firefox
+@browser = Watir::Browser.new $browser_type.to_sys #convert to symbol
 end
 
 After do |scenario|
